9cc0fc3a59e8fc537ffa94189e0b35406672c64a
ekdus04 <87374713+ekdus04@users.noreply.github.com>
2023-11-03 21:05:38 +0900

    Add files via upload

---
+++ hw6.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void)
+{
+    int arr[5];
+    printf("Please input five integers: ");
+    for (int i = 0; i < 5; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    printf("\nOdd numbers: ");
+    for (int i = 0; i < 5; i++)
+    {
+        if (arr[i] % 2 == 1)
+            printf("%d ", arr[i]);
+    }
+    printf("\nEven numbers: ");
+    for (int i = 0; i < 5; i++)
+    {
+        if (arr[i] % 2 == 0)
+            printf("%d ", arr[i]);
+    }
+    return 0;
+}
